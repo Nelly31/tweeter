7c4995e7daa05cd13e8653ae65a6f5e2907481b4
Nelly31 <nelly_main@hotmail.com>
2019-07-17 21:25:22 +0000

    newtweet counter fix

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -38,14 +38,13 @@ const createTweetElement = (tweetObj) => {
   return $postContainer;
 };
 
-//rendering the tweets from hard copy data
+//rendering the tweets 
 const renderTweets = (allTweetObj) => {
 
   for (const oneTweetObj of allTweetObj) {
     const $tweet = createTweetElement(oneTweetObj)
-    $('#tweet-container').append($tweet);
+    $('#tweet-container').prepend($tweet);
   }
-
 };
 
 // requesting tweets from localhost8080/tweets
@@ -61,6 +60,35 @@ const loadTweets = () => {
     .fail(error => console.log(error));
 };
 
+//new tweet submission
+$(document).ready(function() {
+
+  $('.new-tweet form').on("submit", function(event) {
+    event.preventDefault();
+
+    let outPut = $(this).serialize();
+
+    let textArea = $('.new-tweet form textarea').val();
+
+    if (textArea === "") {
+      alert('error - your tweet is empty!');
+    } else if (textArea.length > 140) {
+      alert('error - your tweet is too long');
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $(this).serialize()
+      })
+        .then((event) => loadTweets(outPut))
+        .then((event) => {
+          $('.new-tweet form textarea').val('');
+          $('.new-tweet form .counter').text('140');
+        })
+        .fail(error => console.log(error));
+    }
+  });
+});
 
 //checking the page is loaded before calling the function
 $(document).ready(() => {
--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,26 +1,26 @@
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  $('.new-tweet form').on("submit", function(event) {
-    event.preventDefault();
+//   $('.new-tweet form').on("submit", function(event) {
+//     event.preventDefault();
 
-    let outPut = $(this).serialize();
+//     let outPut = $(this).serialize();
 
-    let textArea = $('.new-tweet form textarea').val();
+//     let textArea = $('.new-tweet form textarea').val();
 
-    if (textArea === "") {
-      alert('error - your tweet is empty!');
-    } else if (textArea.length > 140) {
-      alert('error - your tweet is too long');
-    } else {
-      $.ajax({
-        method: 'POST',
-        url: '/tweets',
-        data: $(this).serialize()
-      })
-        .then((event) => console.log(outPut))
-        .fail(error => console.log(error));
-    }
-  });
-});
+//     if (textArea === "") {
+//       alert('error - your tweet is empty!');
+//     } else if (textArea.length > 140) {
+//       alert('error - your tweet is too long');
+//     } else {
+//       $.ajax({
+//         method: 'POST',
+//         url: '/tweets',
+//         data: $(this).serialize()
+//       })
+//         .then((event) => console.log(outPut))
+//         .fail(error => console.log(error));
+//     }
+//   });
+// });
 
