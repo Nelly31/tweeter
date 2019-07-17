08e05c0da13b3c5d4c1bbef4eab8377645320e88
Nelly31 <nelly_main@hotmail.com>
2019-07-17 20:54:23 +0000

    add form validation

--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -5,15 +5,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     let outPut = $(this).serialize();
-    
-    $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: $(this).serialize()
-    })
-      .then((event) => console.log(outPut))
-      .fail(error => console.log(error));
-  });
 
+    let textArea = $('.new-tweet form textarea').val();
+
+    if (textArea === "") {
+      alert('error - your tweet is empty!');
+    } else if (textArea.length > 140) {
+      alert('error - your tweet is too long');
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $(this).serialize()
+      })
+        .then((event) => console.log(outPut))
+        .fail(error => console.log(error));
+    }
+  });
 });
 
